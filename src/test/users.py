331c919a7db8c67ee4c7bb625b2d1c960c1b8ff7
Martina Pauer <martinapauer38@gmail.com>
2025-06-25 14:38:31 +0000

    Second part: The most of code made

--- src/test/users.py
+++ src/test/users.py
@@ -9,16 +9,27 @@ import re
 
 def getComment(text : str) -> bool:
     '''
-        Clasify user comment in 'Hater', 'Fan' or 'Spam'
+        Clasify user comments
     '''
     state = False
     return state
 
 option = 's'
 
+social = Clasify()
+# Get all the comments and who have written it
 while (option.lower() == 's'):
+
     comment = input('Write comment: ')
     name = input('Write user name: ')
+    
+    social.newValue(f'{name} : {comment}')
+
     option = input('Are you wish continue? S/n: ')
+# After of get all the comments clasify each one    
+social.relation()
+
+for text in social.rel.keys():
+    print(f'[ "{text}" is a ({social.rel[text]}) comment ]\n')
 # The path must be complete for get the right file    
 os.system('rm -R /workspaces/clasify/src/__pycache__')
