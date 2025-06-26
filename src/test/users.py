6fcdf35c003dff9afb0853242dc5e849a1da7cf2
Martina Pauer <martinapauer38@gmail.com>
2025-06-26 23:14:53 +0000

    FIXING users.py : encapsulation of categories Use an object apart initialized by condition for set the right category not only the last

--- src/test/users.py
+++ src/test/users.py
@@ -7,18 +7,32 @@ from clasify import Clasify
 
 import re
 
+obj = Clasify()
+
+def rel(val) -> bool:
+    '''
+        Make relation for isolated text
+    '''
+    return True
 def is_that_type(text : str) -> bool:
     '''
         Clasify user comments
     '''
     state = False
 
-    if re.search('odio|hate|maldito|mufa|diabolic|fuck|estúpido|stupid|idiot|morir|muere|idiota|disgust|annoying|asco|shame|blame|vergüenza|mierda|shit|basura|fucking|inútil|die|dead|muerte|kill|matar|destruir|destroy|shit|damn|hell|merde|puta|scheiße|cazzo|mierda|puto|verga|fuck|damn|hell|bastard|idiot|stupid', text):
+    if re.search('odio|hate|maldito|mufa|diabolic|fuck|estúpido|stupid|idiot|morir|muere|idiota|disgust|annoying|asco|shame|blame|vergüenza|mierda|shit|basura|fucking|inútil|die|dead|muerte|kill|matar|destruir|destroy|shit|damn|hell|merde|puta|scheiße|cazzo|mierda|puto|verga|fuck|damn|hell|bastard|idiot|stupid', text.lower()):
     # Hater category
         state = True
-    elif re.search('bien|genial|good|amo|great|love|gran|buen|felicitaciones|congratulations|excellent|excelente|saludos|encant|like|gusta|happy|feliz|amazing|increible|vida|alegr|happiness|vivir|estupendo|maravilloso|gorgeous|gracias|thank|\U0001F44D', text):
+    elif re.search('bien|genial|good|amo|great|love|gran|buen|felicitaciones|congratulations|excellent|excelente|saludos|encant|like|gusta|happy|feliz|amazing|increible|vida|alegr|happiness|vivir|estupendo|maravilloso|gorgeous|gracias|thank|\U0001F44D', text.lower()):
     # Friendly category
-        pass    
+        obj.newType('Friendly')
+        obj.newValue(text)
+        obj.relation(rel)
+    elif re.search('ofrezco|vendo|vender|compra|(haz mi curso)', text.lower()):
+        # Spamer category
+        obj.newType('Spamer')
+        obj.newValue(text)
+        obj.relation(rel)    
 
     return state
 
@@ -40,7 +54,8 @@ while (option.lower() == 's'):
     option = input('\nAre you wish continue? S/n: ')
 # After of get all the comments clasify each one    
 social.relation(is_that_type)
-#social.getRelation('data/comments.csv')
+social.rel.update(obj.rel)
+social.getRelation('data/comments.csv')
 
 print('\nThe user comments are of this way: \n')
 for text in social.rel.keys():
