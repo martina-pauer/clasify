f61a255bb1a7014f929bbe32f656d37ec97588c2
Martina Pauer <martinapauer38@gmail.com>
2025-06-26 19:48:23 +0000

    New Clasifier: ages.py for divide by age in two

---
+++ src/test/ages.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys, os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from clasify import Clasify
+
+def is_age(num):
+    '''
+        Get all values of age and separate
+        by global var age_limit before of
+        be second category
+    '''
+    return (int(num) < age_limit)
+
+age = Clasify()
+
+print('\nClasification by age\n')
+# Get categories and separation age between them
+first_category = input('\tWrite first category: ')
+second_category = input('\tWrite second category: ')
+age_limit = int(input(f'\tHow much years need for be "{second_category}"?: '))
+# Define categories
+age.newType(first_category)
+age.newType(second_category)
+# Show result from 1 to 100 years
+for each in range(1, 101):
+    age.newValue(each)
+# Make relations and output
+age.relation(is_age)
+age.getRelation(f'data/ages/sub-{age_limit}-{first_category}.csv')
+for aging in age.rel.keys():
+    print(f'{aging} years is "{age.rel[aging]}"')   
+# The path must be complete for get the right file    
+os.system('rm -R /workspaces/clasify/src/__pycache__')
