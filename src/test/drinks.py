a4172f9c87a9ad760bdcf1fdf0a10c4ca0fed901
Martina Pauer <martinapauer38@gmail.com>
2025-06-28 21:29:07 +0000

    Set general structure of clasifier demostration

--- src/test/drinks.py
+++ src/test/drinks.py
@@ -45,11 +45,46 @@ class Drinks():
         self.has_bubbles  = bubbles
 
         self.is_hot = temp
+
+objects : list[Drinks] = []
+
+def drinking(name):
+    '''
+        Get drink object by name and set first category
+        that be more unhealthy if at least one logic
+        property is true
+    '''
+    for obj in objects:
+        if name == obj.name:
+            return (obj.has_alcohol or obj.has_bubbles or obj.is_hot)
 # Define drinks categories and data for show the working
 drinking_first = Clasify()
 
+drinking_first.newType('Soda')
+drinking_first.newType('Simple Drink')
+
+water = Drinks('Water', 'transparent')
+
+milk = Drinks('Milk', 'white')
+
+lima = Drinks('Lima', 'transparent')
+lima.setLogic(False, True, False)
+
+for drink in [water, milk, lima]:
+    drinking_first.newValue(drink.name)
+    objects.append(drink)
+
+drinking_first.relation(drinking)
+
 drinking_second = Clasify()
 
+drinking_second.newType('Spirit Drink')
+drinking_second.newType('Juice')
+
 drinking_third = Clasify()
+
+drinking_third.newType('Infusion')
+drinking_third.newType('Juice')
+
 # The path must be complete for get the right file    
 os.system('rm -R /workspaces/clasify/src/__pycache__')
