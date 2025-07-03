10623a591d0b93aad050a400b846676040a9cb51
Martina Pauer <martinapauer38@gmail.com>
2025-07-03 19:34:43 +0000

    Fixing 'country.py': Conditional pairing functions
    Only need the 'european.txt' and the 'ocean.txt'
    list for test it

--- src/test/country.py
+++ src/test/country.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from clasify import Clasify
 
-def ocean(name: str):
+def is_this_america(name: str) -> bool:
     '''
         Select America when is in american list
         elsewhere set Europe
@@ -14,24 +14,46 @@ def ocean(name: str):
     american_list = open('/workspaces/clasify/data/country/american.txt', 'r')
     is_american = False
 
-    for line in american_list.readlines():
-        if line.replace('\n', '') == name:
+    for state in american_list.readlines():
+        if state.replace('\n', '') == name:
             is_american = True
             break
 
+    american_list.close()
+
     return is_american
 
-def age(name: str):
+def is_this_europe(name: str) -> bool:
     '''
         Select Europe or Asia
     '''
-    return not ocean(name)
+    european_list = open('/workspaces/clasify/data/country/european.txt', 'r')
+    is_european = False
 
-def plants(name: str):
+    for member in european_list.readlines():
+        if member.replace('\n', '') == name:
+            is_european = True
+            break
+        
+    european_list.close()    
+
+    return  is_european    
+
+def is_this_africa(name: str) -> bool:
     '''
         Select Africa or Oceania
     '''
-    return not ocean(name)
+    ocean_list = open('/workspaces/clasify/data/country/ocean.txt', 'r')
+
+    is_african = True
+
+    for land in ocean_list.readlines():
+        if land.replace('\n', '') == name:
+            is_african = False
+
+    ocean_list.close()        
+    
+    return  is_african
 
 atlantic = Clasify()
 old = Clasify()
@@ -50,9 +72,9 @@ atlantic.get_values('/workspaces/clasify/data/country/atlantic.txt')
 old.get_values('/workspaces/clasify/data/country/old.txt')
 jungle.get_values('/workspaces/clasify/data/country/jungle.txt')
 # Clasify each one
-atlantic.relation(ocean)
-old.relation(age)
-jungle.relation(plants)
+atlantic.relation(is_this_america)
+old.relation(is_this_europe)
+jungle.relation(is_this_africa)
 
 for each in [atlantic, old, jungle]:
     each.get_relation('/workspaces/clasify/data/country/countries.csv')
