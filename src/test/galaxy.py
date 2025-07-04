e1be0761d42620658abd6d9294ceb9f08e7919b8
Martina Pauer <martinapauer38@gmail.com>
2025-07-04 22:23:15 +0000

    Ending and testing galaxy clasifier
    The weird category works better that common but
    is so aproach for only use the stars count for
    clasify galaxies

--- src/test/galaxy.py
+++ src/test/galaxy.py
@@ -6,6 +6,51 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from clasify import Clasify
 
+def is_common(galaxy: str):
+    '''
+        Select between 'Spiral' or 'Eliptical'
+        galaxy shape counting stars
+    '''
+    stars = float   (
+                        galaxy.replace(' ', '').replace('\n', '').split(':')[1]
+                    )
 
+    return  (
+                stars < 1.2e12
+                and stars > 3.0e9
+            )
+
+def is_weird(galaxy: str):
+    '''
+        Select Irregular or Spiral
+    '''
+    stars = float   (
+                        galaxy.replace(' ', '').replace('\n', '').split(':')[1]
+                    )
+    
+    return  (
+                stars <= 3.0e9
+            )
+# Define clasificators
+common = Clasify()
+weird = Clasify()
+# Set common clasification
+common.new_type('Spiral')
+common.new_type('Eliptical')
+# Set weird clasification
+weird.new_type('Irregular')
+weird.new_type('Spiral')
+# Get values with galaxy names and stars count
+for obj in     [
+                    common,
+                    weird,
+                ]:
+    obj.get_values('/workspaces/clasify/data/galaxy/galaxy.txt')
+# Use two kinds of clasification
+common.relation(is_common)
+weird.relation(is_weird)
+# Fragment clasification
+common.get_relation('/workspaces/clasify/data/galaxy/galactical_common.csv')
+weird.get_relation('/workspaces/clasify/data/galaxy/galactical_weird.csv')    
 # Clean cache
 os.system('rm -R /workspaces/clasify/src/__pycache__')
