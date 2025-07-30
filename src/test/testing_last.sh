90b0fb6fc193def02dce61341e06a0fcdd93367e
Martina Pauer <martinapauer38@gmail.com>
2025-07-30 14:00:17 +0000

    Update testing Bash script
    Add new images to the script test for image color
    from 10 minutes now this script run in 20 minutes
    the script is slow but load all and could be make
    other tasks while this is running

--- src/test/testing_last.sh
+++ src/test/testing_last.sh
@@ -6,5 +6,7 @@ MINIMUM_POINTS=3499525
 ./image_color.py "../../data/colors/heating_map_image-Meta-AI.png" $MINIMUM_POINTS
 ./image_color.py "../../data/colors/heating_map_image-ChatGPT-AI.png" $MINIMUM_POINTS
 ./image_color.py "../../data/colors/heating_map_image-Google_Gemini-AI.png" $MINIMUM_POINTS
-# Show CSV content
+./image_color.py "../../data/colors/natural_image_surrealist-Google_Gemini-AI.png" $MINIMUM_POINTS
+./image_color.py "../../data/colors/tech_image_cubist-Google_Gemini-AI.png" $MINIMUM_POINTS
+# Show CSV content from first to last loaded color info
 cat -n "../../data/colors/image_data.csv"
